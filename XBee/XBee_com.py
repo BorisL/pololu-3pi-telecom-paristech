953a28b54e5dc431c067868be909a37f74f078ef
Liger <boris.liger@gmail.com>
2012-05-24 23:09:04 +0200

    Remove old code from the XBee.py file

--- XBee/XBee_com.py
+++ XBee/XBee_com.py
@@ -8,7 +8,7 @@ if (len(sys.argv) != 3):
     print("Usage: python XBee_com AMQPServerAdress ZigbeeName")
     
 else:
-    serhdl = serial.Serial("/dev/ttyUSB0")
+    serhdl = serial.Serial("/dev/ttyUSB0",timeout=60)
     
     connection = pika.BlockingConnection(pika.ConnectionParameters(
             host=sys.argv[1]))
@@ -19,14 +19,31 @@ else:
     print ' [*] Waiting for messages. To exit press CTRL+C'
 
     def callback(ch, method, properties, body):
-        print " [x] Received %r" % (body,)
-        i =serhdl.write("msg")
-        rep = serhdl.read(2)
-        print rep
+        
+        print " [x] Received from server %r" % (body,)
+           
+        i =serhdl.write(body+".")
+        rep_c = ''
+        rep = ""
+        error = 0
+        while(rep_c != '.'):
+            rep = rep + rep_c
+            rep_c = serhdl.read()
+            
+            #if(rep == ''):
+            #timeout reach
+                #print "\t Timeout reach"
+                #rep_c = '.'
+                #error = 1
+                
+        print " [x] Received from robot %r" % (rep,)
+        if(error == 0):
+            channel.basic_publish(exchange='',
+                      routing_key=rep.rsplit(";")[2],
+                      body=rep)
 
     channel.basic_consume(callback,
                       queue=sys.argv[2],
                       no_ack=True)
 
     channel.start_consuming()
-
